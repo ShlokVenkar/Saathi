b9656f3f9ebfa0ebdd9d0903add3c4df26c3fc13
Khutwad-Bhavesh <b.vs.khutwad@gmail.com>
2026-08-31 22:09:48 +0530

    hardware: add WiFi connection logic

--- hardware/esp32_wristband/esp32_wristband.ino
+++ hardware/esp32_wristband/esp32_wristband.ino
@@ -1,10 +1,25 @@
 // Senior Citizen Helping Wristband - ESP32 Firmware
-// Pin Definitions
+#include <WiFi.h>
+
+const char* ssid = "YOUR_WIFI_SSID";
+const char* password = "YOUR_WIFI_PASSWORD";
+
 const int PIR_PIN = 27;     
 const int BUTTON_PIN = 26;  
 const int BUZZER_PIN = 25;  
 const int GREEN_LED_PIN = 32; 
 const int RED_LED_PIN = 33;   
+
+void connectWiFi() {
+  Serial.print("Connecting to WiFi");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    digitalWrite(RED_LED_PIN, !digitalRead(RED_LED_PIN)); 
+  }
+  digitalWrite(RED_LED_PIN, LOW);
+  digitalWrite(GREEN_LED_PIN, HIGH); 
+}
 
 void setup() {
   Serial.begin(115200);
@@ -13,5 +28,6 @@ void setup() {
   pinMode(BUZZER_PIN, OUTPUT);
   pinMode(GREEN_LED_PIN, OUTPUT);
   pinMode(RED_LED_PIN, OUTPUT);
+  connectWiFi();
 }
 void loop() {}
