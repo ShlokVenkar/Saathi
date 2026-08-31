291dd315d0955efd78b557df1b80b64065e0c8ba
Khutwad-Bhavesh <b.vs.khutwad@gmail.com>
2026-08-31 22:09:48 +0530

    hardware: add HTTP POST functionality for cloud alerts

--- hardware/esp32_wristband/esp32_wristband.ino
+++ hardware/esp32_wristband/esp32_wristband.ino
@@ -1,8 +1,10 @@
 // Senior Citizen Helping Wristband - ESP32 Firmware
 #include <WiFi.h>
+#include <HTTPClient.h>
 
 const char* ssid = "YOUR_WIFI_SSID";
 const char* password = "YOUR_WIFI_PASSWORD";
+String serverName = "http://YOUR_SERVER_IP:PORT/api/alert"; 
 
 const int PIR_PIN = 27;     
 const int BUTTON_PIN = 26;  
@@ -11,23 +13,24 @@ const int GREEN_LED_PIN = 32;
 const int RED_LED_PIN = 33;   
 
 void connectWiFi() {
-  Serial.print("Connecting to WiFi");
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    digitalWrite(RED_LED_PIN, !digitalRead(RED_LED_PIN)); 
-  }
-  digitalWrite(RED_LED_PIN, LOW);
+  while (WiFi.status() != WL_CONNECTED) { delay(500); }
   digitalWrite(GREEN_LED_PIN, HIGH); 
+}
+
+void sendAlertToCloud(String alertType) {
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+    http.begin(serverName);
+    http.addHeader("Content-Type", "application/json");
+    String jsonPayload = "{\"device_id\":\"wristband_01\",\"alert_type\":\"" + alertType + "\"}";
+    int httpResponseCode = http.POST(jsonPayload);
+    http.end();
+  }
 }
 
 void setup() {
   Serial.begin(115200);
-  pinMode(PIR_PIN, INPUT);
-  pinMode(BUTTON_PIN, INPUT_PULLUP); 
-  pinMode(BUZZER_PIN, OUTPUT);
-  pinMode(GREEN_LED_PIN, OUTPUT);
-  pinMode(RED_LED_PIN, OUTPUT);
   connectWiFi();
 }
 void loop() {}
